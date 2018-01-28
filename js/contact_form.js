3f011a2bf2a5d7da8074e2f375e1cbd968a8e9ca
Gaston Rey <gastonrey@gmail.com>
2018-01-28 19:49:05 +0100

    Complefted career section and added some improvements to contact form

--- js/contact_form.js
+++ js/contact_form.js
@@ -55,6 +55,7 @@ form.addEventListener("submit", function(event) {
     return false;
   }
 
+  phone.value = phone.value.replace(/[^+\d]+/g, "");
   var phoneRegex = /((?:\+|00)[17](?: |\-)?|(?:\+|00)[1-9]\d{0,2}(?: |\-)?|(?:\+|00)1\-\d{3}(?: |\-)?)?(0\d|\([0-9]{3}\)|[1-9]{0,3})(?:((?: |\-)[0-9]{2}){4}|((?:[0-9]{2}){4})|((?: |\-)[0-9]{3}(?: |\-)[0-9]{4})|([0-9]{7}))/;
   var isPhoneCorrect = phoneRegex.test(phone.value);
 
